77f6cbabe375646d3ff890e85682d553b85ef953
areumH <94692566+areumH@users.noreply.github.com>
2023-01-09 22:23:16 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,11 @@
+function solution(common) {
+    var answer = 0;
+
+    if ((common[1]-common[0]) == (common[2]-common[1])) {
+        answer = common[common.length-1] + (common[1]-common[0]);
+    } else if ((common[1]/common[0]) == (common[2]/common[1])) {
+        answer = common[common.length-1] * (common[1]/common[0]);
+    }
+    
+    return answer;
+}
