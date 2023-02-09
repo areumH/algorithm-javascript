74dec223024045edf92976a499948d3aedc61be8
areumH <94692566+areumH@users.noreply.github.com>
2023-02-09 22:25:31 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,13 @@
+function solution(a, b) {
+    var answer = 0;
+    if (a>b) {
+        for(let i=b; i<=a; i++) {
+            answer += i;
+        }
+    } else {
+        for(let i=a; i<=b; i++) {
+            answer += i
+        }
+    }
+    return answer;
+}
