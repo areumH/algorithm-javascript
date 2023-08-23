0d72253a0b0462f3c6f6ec862dde7aa92f0dce5d
areumH <94692566+areumH@users.noreply.github.com>
2023-08-23 20:25:24 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,10 @@
+function solution(strings, n) {
+    strings.sort((a,b) => {
+        if (a[n] !== b[n]) {
+            return a[n] > b[n] ? 1 : -1
+        }
+        return a > b ? 1 : -1
+    })
+    
+    return strings;
+}
