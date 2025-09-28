eb4d7f024a62877a3e5d36b20e392490cacf7ad5
아름 <94692566+areumH@users.noreply.github.com>
2025-09-29 01:58:16 +0900

    Time: 2 ms (26.69%), Space: 58.4 MB (25.74%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let x = 1;
+    
+    for(let i=1; i<nums.length; i++) {
+        if(nums[i] !== nums[i-1]) {
+            nums[x] = nums[i];
+            x++;
+        }
+    }
+    
+    return x;
+};
