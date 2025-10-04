c3d0377b535f6443cdb999c476ef6416b386ddda
아름 <94692566+areumH@users.noreply.github.com>
2025-10-04 21:54:57 +0900

    Time: 0 ms (100%), Space: 52.4 MB (98.13%) - LeetHub

---
+++ leetcode/Easy/0027-remove-element/0027-remove-element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let i = 0;
+    let n = nums.length;
+
+    while (i < n) {
+        if(nums[i] === val) {
+            nums[i] = nums[n-1];
+            n--;
+        } else {
+            i++
+        }
+    }
+
+    return n;
+};
